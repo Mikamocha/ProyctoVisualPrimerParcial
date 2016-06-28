f83f50dbefef8cf143605e6468f8baf688654ca3
yandersantana <ysantanatavares.1996@hotmail.com>
2016-06-28 10:59:09 -0700

    Clase Vendedor

---
+++ Proyecto/Proyecto/Vendedor.vb
@@ -0,0 +1,35 @@
+﻿Public Class Vendedor
+    Inherits Persona
+    Private _id As Short
+
+    Public Property Id As Short
+        Get
+            Return _id
+        End Get
+        Set(value As Short)
+            _id = value
+        End Set
+    End Property
+    Private _fechaDeContrato As Date
+    Public Property FechaDeContrato As Date
+        Get
+            Return _fechaDeContrato
+        End Get
+        Set(value As Date)
+            _fechaDeContrato = value
+        End Set
+    End Property
+
+    Private _contacto As String
+
+    Public Property Contacto As String
+        Get
+            Return _contacto
+        End Get
+        Set(value As String)
+            _contacto = value
+        End Set
+    End Property
+
+
+End Class
