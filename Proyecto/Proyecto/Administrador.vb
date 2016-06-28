d47fc27bfd34ca9f8f9052f6d22f5f7d2df48c0e
Yander <Yander@YANDER-PC>
2016-06-28 10:41:46 -0700

    Clase Administrador

---
+++ Proyecto/Proyecto/Administrador.vb
@@ -0,0 +1,12 @@
+﻿Public Class Administrador
+    Inherits Persona
+    Private _nroPersonasAcargo As Integer
+    Public Property NroPersonasAcargo() As Integer
+        Get
+            Return _nroPersonasAcargo
+        End Get
+        Set(ByVal value As Integer)
+            _nroPersonasAcargo = value
+        End Set
+    End Property
+End Class
